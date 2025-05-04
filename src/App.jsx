76cb9f8ba8f4b2813b0bcd6ec7e0db0e943c19a6
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-04 12:26:53 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+// import { sum } from './helper'
 import Lottery from "./Lottery"
 
 function App() {
 
+  let winCondition = (ticket) => {
+    return ticket.every((num) => num === ticket[0])
+  }
+
   return (
     <>
-      <Lottery />
+      <Lottery n={3} winCondition={winCondition} />
     </>
   )
 }
--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,23 +1,18 @@
 import { useState } from "react"
-import "./Lottery.css"
-import { genRandTicket, sum } from "./helper"
+import { genRandTicket } from "./helper"
+import Ticket from "./Ticket"
 
-export default function Lottery() {
-    let [ticket, setTicket] = useState(genRandTicket(3))
-    let isWinning = sum(ticket) === 15;
+export default function Lottery({ n, winCondition }) {
+    let [ticket, setTicket] = useState(genRandTicket(n))
+    let isWinning = winCondition(ticket);
 
     let buyTicket = () => {
-        setTicket(genRandTicket(3))
+        setTicket(genRandTicket(n))
     }
     return (
         <div>
             <h1>Lottery Game!</h1>
-            <div className="ticket">
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
-            </div>
-            <br></br>
+            <Ticket ticket={ticket} />
             <button onClick={buyTicket}>Buy New Ticket</button>
             <h3>{isWinning && "Congratulations You Won!"}</h3>
         </div>
